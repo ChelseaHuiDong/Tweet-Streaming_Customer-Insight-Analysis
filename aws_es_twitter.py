5f78561f2fba44fbd4739cfd6094758630f6677b
ChelseaHuiDong <34147168+ChelseaHuiDong@users.noreply.github.com>
2017-12-02 00:47:46 -0600

    Update aws_es_twitter.py

--- aws_es_twitter.py
+++ aws_es_twitter.py
@@ -6,11 +6,11 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 
 
-consumer_key="H8Fbaj3LBTzCiI1ICTs43bH92"  
-consumer_secret="WXNaRK3TfWFTFvcqhIPoB6IQhyzzPiv9gEkEDFTW55Mb8LjEu2"
+consumer_key="***"  
+consumer_secret="***"
 
-access_token="324458317-wOpvuBl1pXF20Tr3isD83UrR7l21qe7kvIDS5KFA"  
-access_token_secret="iNVfAPRqN7QzvWnGa7GU3W1Vo9IkXIUQ4cJL7DEaxhyLW"
+access_token="***"  
+access_token_secret="***"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
 auth.set_access_token(access_token, access_token_secret)
@@ -19,9 +19,9 @@ auth.set_access_token(access_token, access_token_secret)
 from elasticsearch import RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
-AWS_ACCESS_KEY = 'AKIAJEM2Z2WULFV2RZQA'
-AWS_SECRET_KEY = '+aq3APqeCjpPDUbCWoQ8RTpHNL9+Bu6d8l2e02/g'
-region = 'us-east-1' # For example, us-east-1
+AWS_ACCESS_KEY = '***'
+AWS_SECRET_KEY = '***'
+region = '***' # For example, us-east-1
 service = 'es'
 
 awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, service)
